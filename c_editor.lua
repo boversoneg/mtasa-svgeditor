184461c2b6ea842987a76ec699b98c98d91653b1
bover <59316027+boversoneg@users.noreply.github.com>
2022-05-08 09:54:54 +0200

    Added working SVG generator, fixed square generating

--- c_editor.lua
+++ c_editor.lua
@@ -21,13 +21,14 @@ editor.gui = {}
 editor.gui.window = guiCreateWindow(0.1, 0.2, 0.3, 0.6, 'SVG Editor', true)
 guiWindowSetMovable(editor.gui.window, false)
 guiWindowSetSizable(editor.gui.window, false)
-editor.gui.updateButton = guiCreateButton(0.05, 0.85, 0.425, 0.1, 'Aktualizuj używając\nwprowadzonych wartości', true, editor.gui.window)
-editor.gui.updateButton2 = guiCreateButton(0.525, 0.85, 0.425, 0.1, 'Aktualizuj używając\nraw daty', true, editor.gui.window)
+editor.gui.updateButton = guiCreateButton(0.05, 0.85, 0.425, 0.1, 'Update preview using provided arguments', true, editor.gui.window)
+editor.gui.updateButton2 = guiCreateButton(0.525, 0.85, 0.425, 0.1, 'Update preview using raw data', true, editor.gui.window)
 editor.gui.editWidth = guiCreateEdit(0.05, 0.1, 0.3, 0.05, 200, true, editor.gui.window)
 editor.gui.editHeight = guiCreateEdit(0.05, 0.19, 0.3, 0.05, 200, true, editor.gui.window)
-editor.gui.widthLabel = guiCreateLabel(0.05, 0.07, 0.3, 0.05, 'Szerokość:', true, editor.gui.window)
-editor.gui.heightLabel = guiCreateLabel(0.05, 0.161, 0.3, 0.05, 'Wysokość:', true, editor.gui.window)
-editor.gui.editRound = guiCreateEdit(0.05, 0.27, 0.3, 0.05, 40, true, editor.gui.window)
+editor.gui.widthLabel = guiCreateLabel(0.05, 0.07, 0.3, 0.05, 'Width:', true, editor.gui.window)
+editor.gui.heightLabel = guiCreateLabel(0.05, 0.161, 0.3, 0.05, 'Height:', true, editor.gui.window)
+editor.gui.roundLabel = guiCreateLabel(0.05, 0.252, 0.3, 0.05, 'Round radius:', true, editor.gui.window)
+editor.gui.editRound = guiCreateEdit(0.05, 0.28, 0.3, 0.05, 40, true, editor.gui.window)
 editor.gui.rawDataMemo = guiCreateMemo(0.05, 0.37, 0.9, 0.45, '', true, editor.gui.window)
 
 editor.isMouseIn = function(x, y, w, h)
@@ -39,18 +40,25 @@ editor.isMouseIn = function(x, y, w, h)
     return ((cx >= x and cx <= x + w) and (cy >= y and cy <= y + h))
 end
 
+editor.createRawDataUsingArguments = function(w, h, round, type)
+    local data = editor.generator.tags
+    if type == 'square' then 
+        local openTag = string.sub(data, 0, -8)
+        openTag = openTag ..' '..editor.generator.svgArgs..'>'
+
+        local closeTag = string.sub(data, -6)
+        
+        data = openTag..'\n'..editor.generator.roundedSquare..'\n'..closeTag
+        data = string.format(data, w, h, w, h, w, h, round)
+        return data
+    end 
+end 
+
 editor.updateSVG = function()
     local width = tonumber(guiGetText(editor.gui.editWidth))
     local height = tonumber(guiGetText(editor.gui.editHeight))
     local round = tonumber(guiGetText(editor.gui.editRound))
-    local onePercentRound = 0.447692
-    local rawData = [[
-        <svg width="%s" height="%s" viewBox="0 0 %s %s" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 %sC0 %s %s 0 %s 0H%sC%s 0 %s %s %s %sV%sC%s %s %s %s %s %sH%sC%s %s 0 %s 0 %sV%sZ" fill="#C4C4C4"/>
-        </svg>
-    ]]
-
-    rawData = string.format(rawData, width, height, width, height, round, onePercentRound * round, onePercentRound * round, round, width - round, width - (onePercentRound * round), width, onePercentRound * round, width, round, height - (onePercentRound * round), width, height - (onePercentRound * round), width - (onePercentRound * round), height, width - (onePercentRound * round), height, round, onePercentRound * round, height, height - (onePercentRound * round), height - round, round)
+    local rawData = editor.createRawDataUsingArguments(width, height, round, 'square')
 
     guiSetText(editor.gui.rawDataMemo, rawData)
 
@@ -72,7 +80,7 @@ editor.updateWithRawData = function()
 end 
 
 editor.renderPreview = function()
-    dxDrawText('Podgląd', sx * 0.64, sy * 0.1, sx * 0.65, sy * 0.1, tocolor(255, 255, 255), 5, 'default-bold')
+    dxDrawText('Preview', sx * 0.64, sy * 0.1, sx * 0.65, sy * 0.1, tocolor(255, 255, 255), 5, 'default-bold')
     dxDrawImage(editor.previewPos[1], editor.previewPos[2], editor.previewPos[3], editor.previewPos[4], editor.svg)
 
     if editor.isMouseIn(editor.previewPos[1], editor.previewPos[2], editor.previewPos[3], editor.previewPos[4]) and getKeyState('mouse1') then 
---
+++ c_generator.lua
@@ -0,0 +1,13 @@
+--[[
+    @Author: Maciej "bover." Grzymkowski
+    Zacznijmy wymagać więcej ~ Xyrusek
+    
+    Kontakt mailowo: biznes.bover@gmail.com
+    Kontakt discord: bover.#1765
+]]--
+
+editor.generator = {}
+
+editor.generator.tags = '<svg></svg>'
+editor.generator.svgArgs = 'width="%s" height="%s" viewBox="0 0 %s %s"'
+editor.generator.roundedSquare = '<rect width="%s" height="%s" rx="%s" />'
